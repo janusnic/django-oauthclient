1f2e8535b711a3e45a31ea8dcd5713bacffbaf72
evan <harrise@carleton.edu>
2015-05-09 14:19:17 -0500

    removed rest dependency

--- google_oauthclient/urls.py
+++ google_oauthclient/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-from oauthclient import views
+from google_oauthclient import views
 
 urlpatterns = [
     url(r'^login', views.login),
     url(r'^revoke',views.revoke, name='revoke'),
     url(r'^oauth2callback', views.oauth2_callback, name='oauth2callback')
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
