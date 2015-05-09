39039b0261e61bf5bef5a8373156a680d844ac89
evan <harrise@carleton.edu>
2015-05-09 14:21:37 -0500

    version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='google_oauthclient',
-    version='1.3',
+    version='1.5',
     packages=['google_oauthclient'],
     include_package_data=True,
     license='MIT',
     description='',
     author='Evan Harris',
     url = 'https://github.com/harrise/django-oauthclient',
-    download_url = 'https://github.com/harrise/django-oauthclient/tarball/1.3', 
+    download_url = 'https://github.com/harrise/django-oauthclient/tarball/1.5', 
     author_email='harrise@carleton.edu',
     classifiers=[
         'Environment :: Web Environment',
